bdb47d3f96a9870dba1318f801586218cdf24cc3
jpkh0ury <jpoliveirakhoury@gmail.com>
2024-06-03 17:03:49 -0300

    criacao dos inputs com o a classe scanner e gerando o output

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o número da conta!");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Digite o número da agência!");
+        String agencia = scanner.next();
+
+        scanner.nextLine();
+
+        System.out.println("Digite seu nome!");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite o valor do saldo!");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        scanner.close();
     }
 }
